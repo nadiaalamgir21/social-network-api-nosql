1f15e21b80fcb714f25ee3f567bbb6046fe61263
nadiaalamgir21 <87517265+nadiaalamgir21@users.noreply.github.com>
2022-02-13 09:42:37 -0800

    comments added in code

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express= require('express');
 const mongoose=require('mongoose');
 
+//create express app server
 const app= express();
+//use port from env variables or 3002
 const PORT = process.env.PORT || 3002;
 
+//server should use json datatype
 app.use(express.json());
+//server should use urlendcoded form of data
 app.use(express.urlencoded({extended:true}));
 app.use(express.static('public'));
 
@@ -15,6 +19,7 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network'
 });
 
 mongoose.set('debug', true);
+//all the routes are contained in routes folder
 app.use(require('./routes'));
 app.listen(PORT,()=> console.log(`Connected to local host`));
 
